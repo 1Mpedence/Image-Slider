a458d79cc6913149d8029e47c948bd781d093b16
Harsh.Anand <Harsh.Anand@ril.com>
2024-11-01 22:55:43 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ImageSliderComponent } from './image-slider/image-slider.component';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImageSliderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatTooltipModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/image-slider/image-slider.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-image-slider',
+  templateUrl: './image-slider.component.html',
+  styleUrls: ['./image-slider.component.scss'],
+})
+export class ImageSliderComponent implements OnInit {
+  imageIdx = 0;
+
+  images = [
+    {
+      id: 0,
+      author: 'Alejandro Escamilla',
+      download_url: 'https://picsum.photos/id/320/5000/3750',
+      left: 0,
+    },
+    {
+      id: 1,
+      author: 'Alejandro Escamilla',
+      download_url: 'https://picsum.photos/id/321/5000/3750',
+      left: 0,
+    },
+    {
+      id: 2,
+      author: 'Alejandro Escamilla',
+      download_url: 'https://picsum.photos/id/322/5000/3750',
+      left: 0,
+    },
+    {
+      id: 3,
+      author: 'Alejandro Escamilla',
+      download_url: 'https://picsum.photos/id/323/5000/3750',
+      left: 0,
+    },
+    {
+      id: 4,
+      author: 'Alejandro Escamilla',
+      download_url: 'https://picsum.photos/id/324/5000/3750',
+      left: 0,
+    },
+  ];
+
+  ngOnInit(): void {
+    this.images.forEach((image, idx) => {
+      image.left = idx * 100;
+    });
+  }
+
+  sliderBtn(dir: -1 | 1) {
+    this.imageIdx = (this.imageIdx + dir + this.images.length) % this.images.length;
+  }
+
+  selectImage(idx: number) {
+    this.imageIdx = idx;
+  }
+}
